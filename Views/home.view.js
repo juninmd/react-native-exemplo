cd2b44e46d53e4834b9d92e7db388ce75b585f5c
Antonio Carlos <antonio.junior@luizalabs.com>
2018-05-30 15:29:19 -0300

    init

---
+++ Views/home.view.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+const Props = {};
+export default class Home extends Component<Props> {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text>Seja bem vindo</Text>
+        <Button title='Conhecer personagens Star Wars' onPress={() => navigate('StarWars')}></Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import { StackNavigator } from 'react-navigation';
 
-AppRegistry.registerComponent('minicurso', () => App);
+import Home from './Views/home.view';
+import StarWars from './Views/star-wars.view'
+
+const routes = StackNavigator({
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            headerTitle: 'Home',
+            headerTintColor: '#ffffff',
+            headerStyle: {
+                backgroundColor: '#2b7cd7',
+            }
+        },
+    },
+    StarsWars: {
+        screen: StarWars,
+        navigationOptions: {
+            headerTitle: 'Personagens',
+            headerTintColor: '#ffffff',
+            headerStyle: {
+                backgroundColor: '#2b7cd7',
+            }
+        }
+    },
+});
+
+AppRegistry.registerComponent('minicurso', () => routes);
